089cd2b17445513c6b907da2046e1fe10a0a7aef
jakhcz <b02902033@csie.ntu.edu.tw>
2019-06-06 15:24:10 -0700

    updates install script

--- remote/aux/install.sh
+++ remote/aux/install.sh
@@ -2,7 +2,8 @@ rm -rf /home/ec2-user/go-workplace/src/github.com/kpister/raft
 
 echo “pull the repo and checking out master branch...”
 go get -u github.com/kpister/fvp/server
-cd github.com/kpister/fvp/server
+cd /home/ec2-user/go-workplace/src/github.com/kpister/fvp/server/
+git checkout global_lock
 go install
 
 cd ~
